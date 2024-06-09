deb6b736b66d1134285fb06846e5e4b36fe5d379
Nitesh Nandan <niteshnitp5686@gmail.com>
2024-06-10 01:29:45 +0530

    Seperatd m2 for test

--- src/test/java/com/nitesh/spring_test_project/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/nitesh/spring_test_project/repository/EmployeeRepositoryTest.java
@@ -9,14 +9,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.event.annotation.BeforeTestExecution;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
+@ExtendWith(SpringExtension.class)
 class EmployeeRepositoryTest {
 
     @Mock
--- src/test/java/com/nitesh/spring_test_project/transitive/ServiceATest.java
+++ src/test/java/com/nitesh/spring_test_project/transitive/ServiceATest.java
@@ -5,10 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 class ServiceATest {
@@ -24,6 +21,5 @@ class ServiceATest {
     void testCheckBeans() {
         Assertions.assertThat(serviceB).isNotNull();
         Assertions.assertThat(serviceA).isNotNull();
-        Assertions.assertThat(serviceB.getServiceC()).isNotNull();
     }
 }
